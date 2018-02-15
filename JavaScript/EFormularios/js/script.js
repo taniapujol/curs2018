c36d0dff7d07af592cfecfb092c7fa8f7df76f95
curs2018 <curs2018@BI101-03.epsevg.upc.es>
2018-02-15 13:46:06 +0100

    esto no funciona

--- JavaScript/EFormularios/js/script.js
+++ JavaScript/EFormularios/js/script.js
@@ -1,13 +1,19 @@
 // Preparando el javascript a la espera de la carga de la página
+window.addEventListener("closed",adios);
 window.addEventListener("load", cargarEventos);
 
 // creacion de la funciones necesarias
 function AlertSubmit() {
     document.getElementById("enviar").addEventListener("click", function () {
         alert("Se Ha enviado el Formularia introducido");
-        document.getElementById("enviar").type="submit";
+        document.getElementById("enviar").type = "submit";
     })
 }
+function adios() {
+    alert("adios, regresa pronto");
+    window.close();
+}
+
 
 // Funcion Principar cargarEventos
 
