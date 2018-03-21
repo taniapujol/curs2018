07d245e64e2aacd26b67b20e3a20cf4a27a68d7d
curs2018 <curs2018@BI101-03.epsevg.upc.es>
2018-03-21 14:03:51 +0100

    asfd

--- PHP/WEB ESTACINA/index.php
+++ PHP/WEB ESTACINA/index.php
@@ -22,7 +22,7 @@
 </body>
     <header>
         <?php
-            include("page/cabecera/$estacion.$extension");
+            include("page/cabecera.$extension");
         ?>
     </header>
     <seccion>
@@ -32,7 +32,7 @@
     </seccion>
     <footer>
         <?php
-            include("page/pie/$estacion.$extension");
+            include("page/pie.$extension");
         ?>
     </footer>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
